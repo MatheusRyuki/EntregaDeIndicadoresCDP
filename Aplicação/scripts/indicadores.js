395b109d129acb60a43e72c8401dfc5bdcfb9984
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2018-05-25 12:57:07 +0000

    Ajuste de Erro de Script dos Indicadores.js

--- Aplicação/scripts/indicadores.js
+++ Aplicação/scripts/indicadores.js
@@ -1,6 +1,6 @@
 $('.tabular.menu .item').tab();
 
 $( document ).ready(function() {
-   $("area").html(localStorage.area);
+   $("#area").html(localStorage.area);
      $("mes").html(localStorage.mes);
 });
