37e0cbe9c3326fdc81d0581aa24b298a12a7e8ba
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2018-05-25 12:16:57 +0000

    Finalizado Script do Index

--- Aplicação/scripts/index.js
+++ Aplicação/scripts/index.js
@@ -4,6 +4,8 @@ $('.ui.dropdown')
 // Clique de ínicio
 
 $("#inicio").click(function(){
-    
+    localStorage.mes = $( "#mes option:selected" ).text();
+    localStorage.area = $( "#area option:selected" ).text();
+    window.location.href = "./views/indicadores.html";
 });
 
