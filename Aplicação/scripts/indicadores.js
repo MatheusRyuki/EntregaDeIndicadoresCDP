a859a84137f024f91bdf1b9983a7d8a529690a9f
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2018-05-28 12:10:51 +0000

    Adicionado ASSCOM em indicadores automático

--- Aplicação/scripts/indicadores.js
+++ Aplicação/scripts/indicadores.js
@@ -3,10 +3,20 @@ $('.tabular.menu .item').tab();
 $( document ).ready(function() {
    $("#area").html(localStorage.area);
 $("#mes").html(localStorage.mes);
+
+//Teste de criação
+    //Se for ASSCOM
+        if(localStorage.area == 'ASSCOM') {
+            $("#estrategicos").html(
+'<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Agenda do plano de comunicação" class="indicador3"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado3"> </div></div> </div><button class="ui primary button" id="estrategicosButton" > Salvar</button>');
+            $("#processos").html('<div class="ui info message"><div class="header">Sem Indicadores</div><ul class="list"><li>Não há indicadores desse cenário para essa área.</li></ul></div>')
+             $("#setorial").html('<div class="ui info message"><div class="header">Sem Indicadores</div><ul class="list"><li>Não há indicadores desse cenário para essa área.</li></ul></div>')
+             }
+
 });
 
 
-<!--Clique no botão de Processos-->
+//Clique no botão de Processos
 
 $("#processoButton").click(function(){
     
