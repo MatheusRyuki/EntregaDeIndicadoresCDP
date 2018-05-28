c619aae3a006317d357a06242ef77f95e84584d9
Matheus Ryuki Medeiros Kaiya <dr_4_gao@hotmail.com>
2018-05-28 14:14:31 +0000

    Adicionado SIC e SUCOFA

--- Aplicação/scripts/indicadores.js
+++ Aplicação/scripts/indicadores.js
@@ -69,14 +69,24 @@ $("#mes").html(localStorage.mes);
 + '<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Satisfação do atendimento" class="indicador1"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado1"> </div></div> </div>'
 + '<button class="ui primary button" id="processoButton" > Salvar</button>');
         } //Se for SIC
-
-
-
-
-
-
-
-
+            else if (localStorage.area == 'SIC') {
+                 $("#estrategicos").html('<div class="ui info message"><div class="header">Sem Indicadores</div><ul class="list"><li>Não há indicadores desse cenário para essa área.</li></ul></div>')
+                 $("#processos").html('<div class="ui info message"><div class="header">Sem Indicadores</div><ul class="list"><li>Não há indicadores desse cenário para essa área.</li></ul></div>')
+                  $("#setorial").html(
+'<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Índice de atendimento em até 30 dias" class="indicador2"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado2"> </div></div> </div>'
++ '<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Índice de resposta aos pedidos de informação" class="indicador2"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado2"> </div></div> </div>'
++ '<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Tempo médio de resposta aos pedidos de informação" class="indicador2"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado2"> </div></div> </div>'
++ '<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Proporção por modalidade(SIC)" class="indicador2"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado2"> </div></div> </div>'
++ '<button class="ui primary button" id="setorialButton" > Salvar</button>');
+        } //Se for SUCOFA
+            else if (localStorage.area == 'SUCOFA') {
+                 $("#estrategicos").html('<div class="ui info message"><div class="header">Sem Indicadores</div><ul class="list"><li>Não há indicadores desse cenário para essa área.</li></ul></div>')
+                 $("#processos").html('<div class="ui info message"><div class="header">Sem Indicadores</div><ul class="list"><li>Não há indicadores desse cenário para essa área.</li></ul></div>')
+                 $("#setorial").html('<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Execução do Orçamento de Investimento" class="indicador2"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado2"> </div></div> </div>'
++ '<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Orçamento realizado (PDG) - Dispêndio" class="indicador2"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado2"> </div></div> </div>'
++ '<div class="two fields"><div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"><div class="ui label">Indicador</div><input readonly="" type="text" value="Orçamento realizado (PDG) - Recurso" class="indicador2"></div></div> <div class="field"><!--Área de Inserimento dos dados--><div class="ui labeled input"> <div class="ui label"> Resultado do Indicador </div><input type="text" placeholder="Resultado" class="resultado2"> </div></div> </div>'
++ '<button class="ui primary button" id="setorialButton" > Salvar</button>');
+        }
 
 
 
